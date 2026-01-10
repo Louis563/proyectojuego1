ef13445fe324a6348ae2b5fabd7a29270936c8bf
Lu1s2556 <luiserojasp06@gmail.com>
2026-01-09 21:52:11 -0400

    despuracion de contenido no usado

--- data/DataPlayer.gd
+++
@@ -1,8 +0,0 @@
-class_name DataPlayer  extends Resource
-@export var jugador: bool = true
-
-@export var salud_maxima : float = 100.0
-@export var armadura : float = 1.0
-
-@export var damage : float = 10.0
-@export var multiplicador: float = 1.1

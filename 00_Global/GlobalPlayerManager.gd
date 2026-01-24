a9c2bcd9226d98883423f47924524fcf2d73b4ef
Louis563 <gordovizcalla@gmail.com>
2026-01-24 18:00:09 -0400

    un poco de cambios
    Espero que no se rompa el git XD

--- 00_Global/GlobalPlayerManager.gd
+++ 00_Global/GlobalPlayerManager.gd
@@ -1,3 +1,15 @@
 extends Node
 
 var jugador : Jugador
+
+func set_player_position ( _new_pos : Vector2 ) -> void:
+	jugador.global_position = _new_pos
+	pass
+
+func set_as_parent( _p : Node2D ) -> void:
+	if jugador.get_parent():
+		jugador.get_parent().remove_child( jugador )
+	_p.add_child( jugador )
+
+func unparent_player( _p : Node2D ) -> void:
+	_p.remove_child( jugador )
--- Mapas/bosque.gd
+++ Mapas/bosque.gd
@@ -1,1 +1,5 @@
-extends Node2D
+class_name Bosque extends Node2D
+
+func _ready() -> void:
+    self.y_sort_enabled = true
+    PlayerManager.set_as_parent(self)

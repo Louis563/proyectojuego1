5abcde7814a6aa73eb94ccea2a60f9858261b2b2
Jose Acosta <jq523838@gmail.com>
2026-01-26 18:58:12 -0400

    Agregué el menu principal

--- Assets/Interfaz de usuario/UI PIXEL ART IA/indicador_de_daño.gd
+++ Assets/Interfaz de usuario/UI PIXEL ART IA/indicador_de_daño.gd
@@ -4,12 +4,12 @@ extends Node2D
 @onready var anim: AnimationPlayer = $AnimationPlayer
 
 func mostrar_daño(valor: int, color: Color = Color.RED) -> void:
-    label.text = str(valor)
+	label.text = str(valor)
 
-    # Forzar color del texto, ignorando el Theme
-    label.add_theme_color_override("font_color", color)
+	# Forzar color del texto, ignorando el Theme
+	label.add_theme_color_override("font_color", color)
 
-    # Opcional: por si usas modulate en el nodo
-    label.self_modulate = Color.WHITE
+	# Opcional: por si usas modulate en el nodo
+	label.self_modulate = Color.WHITE
 
-    anim.play("flotar")
+	anim.play("flotar")
